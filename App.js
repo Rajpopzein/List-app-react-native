f1e42d2e09ea3690dd166738a31d72967d0d095a
rajpopzein <kraj0132@gmail.com>
2023-07-03 12:31:46 +0530

    added images

--- App.js
+++ App.js
@@ -7,65 +7,72 @@ import {
   Button,
   FlatList,
   TouchableHighlight,
+  Image,
 } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
   const [taskList, setTaskList] = useState([]);
   const [taskerr, setTaskerr] = useState("Add your list");
-  const [visibilityModel, setVisibilityModel] = useState(false)
+  const [visibilityModel, setVisibilityModel] = useState(false);
 
-  const handle_model_visibility = () =>{
-      setVisibilityModel(true)
-  }
-  const handle_endmodel_visibility = () =>{
-    setVisibilityModel(false)
-  }
+  const handle_model_visibility = () => {
+    setVisibilityModel(true);
+  };
+  const handle_endmodel_visibility = () => {
+    setVisibilityModel(false);
+  };
   const handle_click = (text) => {
     if (text.length > 0 && text !== " ") {
-      const data = [
-        ...taskList,
-        { text: text, id: Math.random().toString() },
-      ];
+      const data = [...taskList, { text: text, id: Math.random().toString() }];
       setTaskList(data);
-      handle_endmodel_visibility()
+      handle_endmodel_visibility();
     } else {
       setTaskerr("Enter a Item");
     }
-   
   };
 
   const delitem = (id) => {
-      setTaskList((taskList)=>{
-      return taskList.filter((item)=>{ return item.id!== id} )
-    })
+    setTaskList((taskList) => {
+      return taskList.filter((item) => {
+        return item.id !== id;
+      });
+    });
   };
 
-  useEffect(()=>{
-
-  },[taskList])
+  useEffect(() => {}, [taskList]);
 
   return (
-    <View style={styles.container}>
-      <Button title={"Add tasks"} color={"#394867"} onPress={handle_model_visibility}/>
-      <GoalInput  handle_click={handle_click} err={taskerr} visible={visibilityModel} voff={handle_endmodel_visibility}/>
-      <View style={styles.goals_container}>
-        <FlatList
-          data={taskList}
-          renderItem={({ item, index, separators }) => {
-            return (
-               <GoalItem data={item} deletes={delitem} id={item.id}/>
-            );
-          }}
-          keyExtractor={(item , index)=>{
-              return(
-                item.id
-              )
-          }}
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Image style={styles.image} source={require('./assets/images/planning.png')}/>
+        <Button
+          title={"Add tasks"}
+          color={"#394867"}
+          onPress={handle_model_visibility}
         />
+        <GoalInput
+          handle_click={handle_click}
+          err={taskerr}
+          visible={visibilityModel}
+          voff={handle_endmodel_visibility}
+        />
+        <View style={styles.goals_container}>
+          <FlatList
+            data={taskList}
+            renderItem={({ item, index, separators }) => {
+              return <GoalItem data={item} deletes={delitem} id={item.id} />;
+            }}
+            keyExtractor={(item, index) => {
+              return item.id;
+            }}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -74,12 +81,19 @@ const styles = StyleSheet.create({
     paddingTop: 50,
     paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#9BA4B5",
     fontSize: 25,
-  
+    // justifyContent:'center',
+    flexDirection:"column",
   },
   goals_container: {
     flex: 5,
   },
-  
+  image:{
+    width:150,
+    height:150,
+    marginLeft:'25%',
+    flex:1,
+    paddingBottom:'10%',
+    // alignItems:'center'
+  },
 });
